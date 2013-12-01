9e20e6f2703f2a9528c2ba51415dff3e586c582d
issei-vaio <issei3478@gmail.com>
2013-12-01 13:02:10 +0900

    add

--- PointGrabber/grabber_main.cpp
+++ PointGrabber/grabber_main.cpp
@@ -1,11 +1,30 @@
-﻿#include "StdAfx.h"
-#include <pcl/io/openni_grabber.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/io/pcd_io.h>
-#include <string>
-#include <iostream>
+﻿#include "main.h"
 
 using namespace std;
+
+void segmentate(pcl::PointCloud<pcl::PointXYZRGBA>& cloud, double threshould)
+{
+	static pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	static pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+	// Create the segmentation object
+	pcl::SACSegmentation<pcl::PointXYZRGBA> seg;
+	// Optional
+	seg.setOptimizeCoefficients (true);
+	// Mandatory
+	seg.setModelType (pcl::SACMODEL_PLANE);
+	seg.setMethodType (pcl::SAC_RANSAC);
+	seg.setDistanceThreshold (threshould);
+
+	seg.setInputCloud (cloud.makeShared ());
+	seg.segment (*inliers, *coefficients);
+
+	for (size_t i = 0; i < inliers->indices.size (); ++i) {
+		cloud.points[inliers->indices[i]].r = 255;
+		cloud.points[inliers->indices[i]].g = 0;
+		cloud.points[inliers->indices[i]].b = 0;
+	}
+}
 
 class SimpleOpenNIViewer
 {
@@ -15,7 +34,19 @@ public:
 	void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
 	{
 		if (!viewer.wasStopped()){
-			viewer.showCloud (cloud);
+			static pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_pass_filterd (new pcl::PointCloud<pcl::PointXYZRGBA>);
+			static pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_down_filtered (new pcl::PointCloud<pcl::PointXYZRGBA>);
+			// はずれ値除去フィルタ
+			this->pass.setInputCloud (cloud);
+			///pass.setFilterLimitsNegative (true);
+			this->pass.filter (*cloud_pass_filterd);
+			// ダウンサンプリングフィルタ
+			this->sor.setInputCloud (cloud_pass_filterd);
+			this->sor.filter (*cloud_down_filtered);
+			// 平面抽出
+			::segmentate( *cloud_down_filtered, 0.005 );
+			// 表示
+			viewer.showCloud (cloud_down_filtered);
 		}
 		//pcl::io::savePCDFileBinary("frame.pcd", *cloud);
 	}
@@ -31,9 +62,15 @@ public:
 
 		grab->start ();
 
+		// 外れ値除去フィルタの設定
+		this->pass.setFilterFieldName ("z");
+		this->pass.setFilterLimits (0.0, 0.8);
+		// ダウンサンプリングフィルタの設定
+		this->sor.setLeafSize (0.01f, 0.01f, 0.01f);
+
 		while (!viewer.wasStopped())
 		{
-			
+
 			//boost::this_thread::sleep( boost::posix_time::milliseconds(2000) ); 
 		}
 
@@ -41,7 +78,10 @@ public:
 	}
 
 	pcl::visualization::CloudViewer viewer;
+	pcl::PassThrough<pcl::PointXYZRGBA> pass; // 外れ値除去フィルタ
+	pcl::VoxelGrid< pcl::PointXYZRGBA > sor; // ダウンサンプリングフィルタ
 };
+
 
 int main ()
 {
--- PointGrabber/stdafx.h
+++ PointGrabber/stdafx.h
@@ -9,7 +9,20 @@
 
 #include <stdio.h>
 #include <tchar.h>
+#include <iostream>
+#include <string>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/openni_grabber.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/filters/approximate_voxel_grid.h>
 
 
 // TODO: プログラムに必要な追加ヘッダーをここで参照してください。
